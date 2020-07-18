ea3e7d3d7925a8aac3ea9e202a92dd7698e117c9
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2020-07-18 12:30:03 -0300

    EnumProcessModules

--- psapi.go
+++ psapi.go
@@ -5,6 +5,7 @@
 package w32
 
 import (
+	"reflect"
 	"syscall"
 	"unsafe"
 )
@@ -12,7 +13,8 @@ import (
 var (
 	modpsapi = syscall.NewLazyDLL("psapi.dll")
 
-	procEnumProcesses = modpsapi.NewProc("EnumProcesses")
+	procEnumProcesses        = modpsapi.NewProc("EnumProcesses")
+	procEnumProcessModulesEx = modpsapi.NewProc("EnumProcessModulesEx")
 )
 
 func EnumProcesses(processIds []uint32, cb uint32, bytesReturned *uint32) bool {
@@ -22,4 +24,28 @@ func EnumProcesses(processIds []uint32, cb uint32, bytesReturned *uint32) bool {
 		uintptr(unsafe.Pointer(bytesReturned)))
 
 	return ret != 0
+}
+
+func EnumProcessModules(hProcess HANDLE, filterFlags DWORD) ([]HMODULE, error) {
+	modules := make([]HMODULE, 128)
+	size := uintptr(0)
+	for {
+		modulesSize := uintptr(len(modules)) * reflect.TypeOf(modules).Elem().Size()
+		ret, _, _ := procEnumProcessModulesEx.Call(
+			uintptr(hProcess),
+			uintptr(unsafe.Pointer(&modules[0])),
+			modulesSize,
+			uintptr(unsafe.Pointer(&size)),
+			uintptr(filterFlags),
+		)
+		if ret == 0 {
+			return nil, syscall.Errno(GetLastError())
+		}
+		retSize := size / reflect.TypeOf(modules).Elem().Size()
+		if size > modulesSize {
+			modules = make([]HMODULE, retSize)
+			continue
+		}
+		return modules[:retSize], nil
+	}
 }
